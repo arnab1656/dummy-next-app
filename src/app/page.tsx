e29d20a41231431922269e83f45d608907a4a5a8
unknown <arnab.paul.1656@gmail.com>
2024-06-03 12:50:06 +0530

    The change in fe and updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Arnab Here from the Dummy NextApp.js
+      Arnab Here from the Dummy NextApp.js and The App is Updated.
     </main>
   );
 }
